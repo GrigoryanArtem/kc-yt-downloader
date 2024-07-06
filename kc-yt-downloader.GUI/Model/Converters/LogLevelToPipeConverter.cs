b477c4eaf27e3147ce2a11a29cb1ac5b38867535
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2024-07-06 14:36:59 +0300

    Added pipe converter

---
+++ kc-yt-downloader.GUI/Model/Converters/LogLevelToPipeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using static kc_yt_downloader.GUI.Model.LogPersister;
+
+namespace kc_yt_downloader.GUI.Model.Converters
+{
+    public class LogLevelToPipeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not LogLevel)
+                return value;
+
+            return (LogLevel)value switch
+            {
+                LogLevel.Standard => "pipe:1",
+                LogLevel.Error => "pipe:2",
+
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
